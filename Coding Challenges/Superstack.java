be543b2f846a58c09e1d73b6ddafa6e0eab61542
Daria Bazueva <bazuevad@seas.upenn.edu>
2021-01-09 14:46:54 -0500

    Added Superstack Question

---
+++ Coding Challenges/Superstack.java
@@ -0,0 +1,16 @@
+public String Superstack(String[] operations){
+    Stack<Integer> stack = new Stack<>();
+    for(int i = 0; i < operations.length;i++){
+        String[] splitted = operations[i].split(" ");
+        if(splitted[0].compareTo("push")==0){
+
+        }
+        else if(splitted[0].compareTo("pop")==0){
+
+        }
+        else if(splitted[0].compareTo("inc")==0){
+
+        }
+    }
+    }
+    
